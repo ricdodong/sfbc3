b6aa3812a58249d7851b9ac87f107ebed3cd6de5
Ric Dodong <ricalgen@gmail.com>
2026-04-03 17:24:58 +0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,20 @@
 import { atom } from 'nanostores';
 
-export const $currentTrack = atom(null); // e.g., "https://www.youtube.com/watch?v..."
-export const $isPlaying = atom(false);
+// The Playlist Array
+export const $playlist = atom([
+  {
+    title: "Sunday Service - Worship",
+    url: "https://www.youtube.com/watch?v=CaTeyPHbY1k",
+    type: "video"
+  },
+  {
+    title: "Sermon 1 - The Word of God",
+    url: "/sermon1.mp3",
+    type: "audio"
+  }
+]);
+
+// Initial State: Load the first item from the playlist by default
+export const $currentTrack = atom("https://www.youtube.com/watch?v=CaTeyPHbY1k");
+export const $trackTitle = atom("Sunday Service - Worship");
+export const $isPlaying = atom(false); // Set to true if you want it to autoplay
